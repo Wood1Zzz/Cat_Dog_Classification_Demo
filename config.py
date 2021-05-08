15f1a08da2963d9256afcf61aab5ad8aae12e725
Danzer <455349235@qq.com>
2021-05-08 14:45:44 +0800

    kaggle

--- config.py
+++ config.py
@@ -10,13 +10,21 @@ RECORD_EPOCH = 5
 LR = 0.0001
 NET = '16'
 ONE_HOT = False
+DEVICE = "kaggle"
 
 # Dataset path for myself
-if sys.platform.startswith('win'):
-    TRAIN_PATH = 'G:\dogs-vs-cats-redux-kernels-edition\\train'
-    VALID_PATH = 'G:\dogs-vs-cats-redux-kernels-edition\\test'
-    DATASET_PATH = 'G:\dogs-vs-cats-redux-kernels-edition'
-elif sys.platform.startswith('linux'):
-    TRAIN_PATH = '/home/danzer/PycharmProject/Dataset/dogs-vs-cats-redux-kernels-edition/train'
-    VALID_PATH = '/home/danzer/PycharmProject/Dataset/dogs-vs-cats-redux-kernels-edition/test'
-    DATASET_PATH = '/home/danzer/PycharmProject/Dataset/dogs-vs-cats-redux-kernels-edition'
+if DEVICE == "my":
+    if sys.platform.startswith('win'):
+        TRAIN_PATH = 'G:\dogs-vs-cats-redux-kernels-edition\\train'
+        VALID_PATH = 'G:\dogs-vs-cats-redux-kernels-edition\\test'
+        DATASET_PATH = 'G:\dogs-vs-cats-redux-kernels-edition'
+    elif sys.platform.startswith('linux'):
+        TRAIN_PATH = '/home/danzer/PycharmProject/Dataset/dogs-vs-cats-redux-kernels-edition/train'
+        VALID_PATH = '/home/danzer/PycharmProject/Dataset/dogs-vs-cats-redux-kernels-edition/test'
+        DATASET_PATH = '/home/danzer/PycharmProject/Dataset/dogs-vs-cats-redux-kernels-edition'
+elif DEVICE == "kaggle":
+    TRAIN_PATH = './cat_dog_classification/train'
+    VALID_PATH = './cat_dog_classification/test'
+    DATASET_PATH = './cat_dog_classification'
+elif DEVICE == "colab":
+    pass
