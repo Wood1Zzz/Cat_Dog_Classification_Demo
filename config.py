dfed1f61cf057fbc5a6f28b1c5ad1af34163ecd1
Danzer <455349235@qq.com>
2021-05-10 15:08:42 +0800

    'push'

--- config.py
+++ config.py
@@ -11,22 +11,26 @@ RECORD_EPOCH = 5
 LR = 0.0001
 NET = '16'
 ONE_HOT = False
-DEVICE = "colab"
 
-# Dataset path for myself
-if DEVICE == "my":
+
+# Special config or dataset path for different device
+DEVICE = os.system('whoami')
+if DEVICE == "desktop-sq6jprd\danzer":
     if sys.platform.startswith('win'):
         TRAIN_PATH = 'G:\dogs-vs-cats-redux-kernels-edition\\train'
         VALID_PATH = 'G:\dogs-vs-cats-redux-kernels-edition\\test'
         DATASET_PATH = 'G:\dogs-vs-cats-redux-kernels-edition'
+        BATCH_SIZE = 50
     elif sys.platform.startswith('linux'):
         TRAIN_PATH = '/home/danzer/PycharmProject/Dataset/dogs-vs-cats-redux-kernels-edition/train'
         VALID_PATH = '/home/danzer/PycharmProject/Dataset/dogs-vs-cats-redux-kernels-edition/test'
         DATASET_PATH = '/home/danzer/PycharmProject/Dataset/dogs-vs-cats-redux-kernels-edition'
+        BATCH_SIZE = 50
 elif DEVICE == "kaggle":
     TRAIN_PATH = '../input/dogs-vs-cats/train/train'
     VALID_PATH = '../input/dogs-vs-cats/test/test'
     DATASET_PATH = '../input/dogs-vs-cats'
+    BATCH_SIZE = 100
 elif DEVICE == "colab":
     os.system(r'pip install -U -q kaggle')
     os.system(r'mkdir -p ~/.kaggle')
@@ -46,3 +50,4 @@ elif DEVICE == "colab":
     TRAIN_PATH = '/content/cat_dog_dataset/train'
     VALID_PATH = '/content/cat_dog_dataset/test'
     DATASET_PATH = '/content/cat_dog_dataset'
+    BATCH_SIZE = 100
