f32d1c535c4d233c3e70925cdb264638bfa1c8b3
Danzer <455349235@qq.com>
2021-05-12 11:32:27 +0800

    valid dataset

--- train.py
+++ train.py
@@ -9,6 +9,7 @@ from tqdm import tqdm
 # import sys
 from config import *
 import os
+from utils import evaluate_accuracy
 
 
 # cat_dog_dataset = dataloader.CatVsDogDataset(TRAIN_PATH)
@@ -56,10 +57,10 @@ def train():
     cat_dog_dataset = dataloader.CatVsDogDataset(TRAIN_PATH, mode="train", one_hot=ONE_HOT)
     train_loader = Data(cat_dog_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=0)
     cat_dog_dataset_test = dataloader.CatVsDogDataset(TRAIN_PATH, mode="test")
-    test_iter = Data(cat_dog_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=0)
+    test_loader = Data(cat_dog_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=0)
     # valid_dataset = dataloader.ValidDataset(VALID_PATH)
     # valid_loader = DataLoader(valid_dataset, batch_size=1, shuffle=False, num_workers=0)
-
+    start_time = time.time()
     print("Net: VGG%s, Total epoch: %d, BATCH_SIZE: %d, LR: %f"%(NET, EPOCH, BATCH_SIZE, LR))
     time.sleep(0.1)
 
@@ -99,14 +100,20 @@ def train():
             else:
                 train_loss_sum += loss.item()
                 train_acc_sum += (torch.round(y_hat) == y).float().mean().item()
-
+            
             # print(train_loss_sum)
             # print(train_acc_sum)
             # train_loss_sum += float(loss_func(y_hat, y))
-
-        print('epoch: {epoch}, loss:{loss}, accuracy:{accuracy}, average_loss:{average_loss}, average_accuracy:{average_accuracy}%'.\
-            format(epoch=epoch, loss=float('%.6f' % train_loss_sum), accuracy=float('%.6f' % train_acc_sum), \
+            
+        print('Epoch: {epoch}, Loss:{loss}, Accuracy:{accuracy}, Average_loss:{average_loss}, Average_accuracy:{average_accuracy}%'.\
+            format(epoch=epoch+1, loss=float('%.6f' % train_loss_sum), accuracy=float('%.6f' % train_acc_sum), \
                 average_loss=float('%.6f' %(train_loss_sum/(batch+1))), \
                     average_accuracy=float('%.6f' % (train_acc_sum/(batch+1)*100))))
+    end_time = time.time()
+    print("Total trainning time:{:.2f}s".format(end_time-start_time))
+    start_time = time.time()
+    valid_acc = evaluate_accuracy(test_loader, net)
+    end_time = time.time()
+    print("Valid accuracy {:.6f}%, Eval time {:.2f}s".format(valid_acc*100, end_time-start_time))
 
 train()
--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import torch
+
 class DatasetModeError(Exception):
     def __init__(self, mode):
         super(DatasetModeError, self).__init__()
@@ -10,3 +12,10 @@ class DatasetModeError(Exception):
 def get_labels(label_digital):
     label_map = ['cat', 'dog']
     return [label_map[int(i)] for i in label_digital]
+
+def evaluate_accuracy(test_iter, net):
+    acc_sum, n = 0.0, 0
+    for x, y in test_iter:
+        acc_sum += (torch.round(net(x)) == y).float().sum().item()
+        n += y.shape[0]
+    return acc_sum / n
